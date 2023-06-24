c07be9077a6d6727ba5169d6ba60b83e565e3bcd
Mohamed Khaled <mohamedkhaled4053@gmail.com>
2023-06-25 00:57:39 +0300

    try mac

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
 let express = require("express");
-const DeviceDetector = require("node-device-detector");
-const ClientHints = require("node-device-detector/client-hints");
-
-const Detector = require("device-detector-js");
-
+const os = require("os");
+const device = require("device");
 let app = express();
 
+function getMacAddress() {
+  const networkInterfaces = os.networkInterfaces();
+  const interfaceNames = Object.keys(networkInterfaces);
+
+  for (const name of interfaceNames) {
+    const networkInterface = networkInterfaces[name];
+    const interfaceWithMac = networkInterface.find(
+      (interfaceData) =>
+        !interfaceData.internal && interfaceData.mac !== "00:00:00:00:00:00"
+    );
+
+    if (interfaceWithMac) {
+      return interfaceWithMac.mac;
+    }
+  }
+
+  return null;
+}
+
 app.get("/one", (req, res) => {
-  const detector = new DeviceDetector({
-    clientIndexes: true,
-    deviceIndexes: true,
-    deviceAliasCode: true,
+  let ip = req.ip;
+  let ips = req.ips;
+  let userAgent = req.headers["user-agent"];
+  const userDevice = device(req.headers["user-agent"]);
+  res.status(200).json({
+    ip,
+    ips,
+    userAgent,
+    mac: getMacAddress(),
+    networkInterfaces: os.networkInterfaces(),
+    userDevice,
   });
-  const clientHints = new ClientHints();
-  const userAgent = req.headers["user-agent"];
-  //@ts-ignore
-  const clientHintData = clientHints.parse(req.headers);
-  const result = detector.detect(userAgent, clientHintData);
-  res.status(200).json({ result });
 });
 
-app.get("/two", (req, res) => {
-    const deviceDetector = new Detector();
-    const userAgent = req.headers["user-agent"];
-    const device = deviceDetector.parse(userAgent);
-  
-    res.status(200).json({device});
-  });
+// app.get("/two", (req, res) => {
+//   res.status(200).json({ device });
+// });
 
 app.listen(8080);
