f6806633c95ec6a2885fa1822f6f249e51e55d02
Mohamed Khaled <mohamedkhaled4053@gmail.com>
2023-06-24 23:22:50 +0300

    detect one

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-let express = require('express')
+let express = require("express");
+const DeviceDetector = require("node-device-detector");
+const ClientHints = require("node-device-detector/client-hints");
 
-let  app = express()
+const Detector = require("device-detector-js");
 
-app.get('/one',(req,res)=>{
-    res.status(200).json({message: 'one'})
-})
+let app = express();
 
-app.listen(8080)
+app.get("/one", (req, res) => {
+  const detector = new DeviceDetector({
+    clientIndexes: true,
+    deviceIndexes: true,
+    deviceAliasCode: true,
+  });
+  const clientHints = new ClientHints();
+  const userAgent = req.headers["user-agent"];
+  //@ts-ignore
+  const clientHintData = clientHints.parse(req.headers);
+  const result = detector.detect(userAgent, clientHintData);
+  res.status(200).json({ result });
+});
+
+app.listen(8080);
