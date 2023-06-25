379515b2a0e47da5ba234c7e73d8730d05a82126
Mohamed Khaled <mohamedkhaled4053@gmail.com>
2023-06-25 12:46:57 +0300

    try ip

--- app.js
+++ app.js
@@ -1,39 +1,16 @@
 let express = require("express");
-const os = require("os");
-const device = require("device");
 let app = express();
 
-function getMacAddress() {
-  const networkInterfaces = os.networkInterfaces();
-  const interfaceNames = Object.keys(networkInterfaces);
-
-  for (const name of interfaceNames) {
-    const networkInterface = networkInterfaces[name];
-    const interfaceWithMac = networkInterface.find(
-      (interfaceData) =>
-        !interfaceData.internal && interfaceData.mac !== "00:00:00:00:00:00"
-    );
-
-    if (interfaceWithMac) {
-      return interfaceWithMac.mac;
-    }
-  }
-
-  return null;
-}
-
 app.get("/one", (req, res) => {
-  let ip = req.ip;
-  let ips = req.ips;
-  let userAgent = req.headers["user-agent"];
-  const userDevice = device(req.headers["user-agent"]);
+  let address = req.socket.address();
+  let remoteAddress = req.socket.remoteAddress;
+  let forwarded = req.headers["x-forwarded-for"];
+  let real = req.headers["x-real-ip"];
   res.status(200).json({
-    ip,
-    ips,
-    userAgent,
-    mac: getMacAddress(),
-    networkInterfaces: os.networkInterfaces(),
-    userDevice,
+    address,
+    remoteAddress,
+    header: forwarded,
+    real
   });
 });
 
